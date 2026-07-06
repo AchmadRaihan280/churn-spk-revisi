69053e44f21852dd3818292d59961e76c1f6c10a
AchmadRaihan280 <achmad280204feb@gmail.com>
2026-07-06 19:03:28 +0700

    Hapus vercel json dan migrasi ke next config rewrites

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async rewrites() {
+    return [
+      {
+        // Ketika di Vercel/Production ada yang menembak ke /api/...,
+        // Next.js akan melemparkannya ke folder api/index.py
+        source: '/api/:path*',
+        destination: '/api/:path*', 
+      },
+    ];
+  },
 };
 
 export default nextConfig;
